6db00f86a20eab66b5393c40208c44e9fdd4b14f
tamarit <tamarit27@gmail.com>
2016-06-18 13:48:26 +0200

    functional-programming-the-sums-of-powers
    https://www.hackerrank.com/challenges/functional-programming-the-sums-of
    -powers

---
+++ functional-programming-the-sums-of-powers/solution.erl
@@ -0,0 +1,95 @@
+% Enter your code here. Read input from STDIN. Print output to STDOUT
+% Your class should be named solution
+
+% https://www.hackerrank.com/challenges/bangalore-bank
+
+-module(solution).
+-export([main/0, prof/0]).
+
+-include_lib("stdlib/include/ms_transform.hrl").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+main() ->
+    Data = read_data(),
+    Res = calculate(Data),
+    output_data(Res),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Profiling
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+prof() ->
+    eprof:start(),
+    eprof:start_profiling([self()]),
+    main2(),
+    eprof:stop_profiling(),
+    eprof:analyze(total). 
+
+main2() ->
+    StartInput = os:timestamp(),
+    Data = read_data(),
+    io:format("INPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartInput)/1000000]),
+    StartRes = os:timestamp(),
+    Res = calculate(Data),
+    io:format("CALCULATE: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartRes)/1000000]),
+    StartOutput = os:timestamp(),
+    output_data(Res),
+    io:format("OUTPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartOutput)/1000000]),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+calculate([X, N]) ->
+    Max = trunc(math:pow(X,1/N)),
+    sums(X, [math:pow(H, N) || H <- lists:seq(1, Max)], 0).
+
+sums(X1, _, X2) when X1 == X2 ->
+    1;
+sums(X1, _, X2) when X1 < X2 ->
+    0;
+sums(X, [H | T], Acc) ->
+        sums(X, T, H + Acc)
+    +   sums(X, T, Acc);
+sums(_, [], _) ->
+    0.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Output
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+output_data(D) -> 
+    io:format("~p\n", [D]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+read_data() ->
+    Binary = read(),
+    Res = binary:split(Binary, [<<"\n">>], [global]),
+    List = [binary_to_list(R) || R <- Res],
+    lists:map(fun str2int/1, List).
+
+str2int(Str) ->
+    element(1,string:to_integer(Str)).
+
+-define(BLK_SIZE, 16384).
+
+read() ->
+    ok = io:setopts(standard_io, [binary]),
+    read(<<>>).
+
+read(Acc) ->
+    case file:read(standard_io, ?BLK_SIZE) of
+        {ok, Data} ->
+            read(<<Acc/bytes, Data/bytes>>);
+        eof ->
+            Acc
+    end.
+
