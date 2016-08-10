249ad7206a9acf25e7da9156adb6101564d48f17
tamarit <tamarit27@gmail.com>
2016-08-10 03:09:37 +0200

    euler018
    https://www.hackerrank.com/contests/projecteuler/challenges/euler018

---
+++ euler018/solution.erl
@@ -0,0 +1,128 @@
+% Enter your code here. Read input from STDIN. Print output to STDOUT
+% Your class should be named solution
+
+% https://www.hackerrank.com/contests/projecteuler/challenges/euler018
+
+-module(solution).
+-export([main/0, prof/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+main() ->
+    Data = read_data(),
+    Res = calculate(Data),
+    output_data(Res),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Profiling
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+prof() ->
+    eprof:start(),
+    eprof:start_profiling([self()]),
+    main2(),
+    eprof:stop_profiling(),
+    eprof:analyze(total). 
+
+main2() ->
+    StartInput = os:timestamp(),
+    Data = read_data(),
+    io:format("INPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartInput)/1000000]),
+    StartRes = os:timestamp(),
+    Res = calculate(Data),
+    io:format("CALCULATE: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartRes)/1000000]),
+    StartOutput = os:timestamp(),
+    output_data(Res),
+    io:format("OUTPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartOutput)/1000000]),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+calculate(Ts) ->
+    lists:map(fun calculate_test/1, Ts).
+
+calculate_test([H|T]) ->
+    case T of 
+        [] ->
+            hd(H);
+        _ ->
+            Pairs = lists:map(fun(E) -> make_pairs(E, []) end, T),
+             hd(H) + choose_max(hd(hd(Pairs)), tl(Pairs))
+    end.
+
+make_pairs([A, B | T], Acc) ->
+    make_pairs([B | T], [{A,B} | Acc]);
+make_pairs([_], Acc) ->
+    lists:reverse(Acc).
+
+choose_max({A, B}, [[PA, PB | _]]) ->
+    % ed({{A, B}, PA, PB}),
+    max(
+        A + choose_max(PA, []),
+        B + choose_max(PB, [])
+    );
+choose_max({A, B}, [[PA, PB | _] | Lines]) ->
+    % ed({{A, B},PA, PB, Lines}),
+    max(
+        A + choose_max(PA, Lines),
+        B + choose_max(PB, [tl(Line) || Line <- Lines])
+    );
+choose_max({A, B}, []) ->
+    % ed({A, B}),
+    max(
+        A,
+        B
+    ).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Output
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+output_data(Ds) -> 
+    lists:map(
+        fun(D) -> io:format("~p\n", [D]) end, 
+        Ds).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+read_data() ->
+    Binary = read(),
+    Res = binary:split(Binary, [<<"\n">>], [global]),
+    [_| Tests] = [binary_to_list(R) || R <- Res],
+    read_tests(Tests, []).
+
+read_tests([LinesStr | T], Acc) ->
+    Lines = str2int(LinesStr),
+    read_tests(string:substr(T, Lines + 1), [lists:map(fun str2intlist/1, string:substr(T, 1, Lines)) | Acc]);
+read_tests([], Acc) ->
+    lists:reverse(Acc).
+
+str2intlist(S) ->
+    [str2int(T) || T <- string:tokens(S, " ")].
+
+str2int(Str) ->
+    element(1,string:to_integer(Str)).
+
+-define(BLK_SIZE, 16384).
+
+read() ->
+    ok = io:setopts(standard_io, [binary]),
+    read(<<>>).
+
+read(Acc) ->
+    case file:read(standard_io, ?BLK_SIZE) of
+        {ok, Data} ->
+            read(<<Acc/bytes, Data/bytes>>);
+        eof ->
+            Acc
+    end.
+
+ed(T) ->    
+    erlang:display(T).
