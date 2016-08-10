a5d04d62ececee82a02c0c3733bc5af64def62f4
tamarit <tamarit27@gmail.com>
2016-08-10 19:44:13 +0200

    number-of-binary-search-tree
    https://www.hackerrank.com/challenges/number-of-binary-search-tree

---
+++ number-of-binary-search-tree/solution.erl
@@ -0,0 +1,103 @@
+% Enter your code here. Read input from STDIN. Print output to STDOUT
+% Your class should be named solution
+
+% https://www.hackerrank.com/challenges/number-of-binary-search-tree
+
+-module(solution).
+-export([main/0, prof/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+main() ->
+    Data = read_data(),
+    Res = calculate(Data),
+    output_data(Res),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Profiling
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+prof() ->
+    eprof:start(),
+    eprof:start_profiling([self()]),
+    main2(),
+    eprof:stop_profiling(),
+    eprof:analyze(total). 
+
+main2() ->
+    StartInput = os:timestamp(),
+    Data = read_data(),
+    io:format("INPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartInput)/1000000]),
+    StartRes = os:timestamp(),
+    Res = calculate(Data),
+    io:format("CALCULATE: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartRes)/1000000]),
+    StartOutput = os:timestamp(),
+    output_data(Res),
+    io:format("OUTPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartOutput)/1000000]),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+calculate(Ts) ->
+    lists:map(fun trees/1, Ts).
+
+trees(1) ->
+    1;
+trees(N) ->
+    cat(N) rem 100000007.
+
+cat(N) -> 
+    FacN = factorial(N),
+    factorial(2 * N) div ((N + 1) * FacN * FacN).
+ 
+factorial(N) -> 
+   fac1(N,1).
+ 
+fac1(0,Acc) -> 
+   Acc; 
+fac1(N,Acc) -> 
+   fac1(N-1, N * Acc).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Output
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+output_data(Ds) -> 
+    lists:map(
+        fun(D) -> io:format("~p\n", [D]) end, 
+        Ds).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+read_data() ->
+    Binary = read(),
+    Res = binary:split(Binary, [<<"\n">>], [global]),
+    [_| Tests] = [binary_to_list(R) || R <- Res],
+    lists:map(fun str2int/1, Tests).
+
+str2int(Str) ->
+    element(1,string:to_integer(Str)).
+
+-define(BLK_SIZE, 16384).
+
+read() ->
+    ok = io:setopts(standard_io, [binary]),
+    read(<<>>).
+
+read(Acc) ->
+    case file:read(standard_io, ?BLK_SIZE) of
+        {ok, Data} ->
+            read(<<Acc/bytes, Data/bytes>>);
+        eof ->
+            Acc
+    end.
+
+ed(T) ->    
+    erlang:display(T).
