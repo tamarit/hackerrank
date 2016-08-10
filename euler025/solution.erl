e7261249c1dc59eb65423bf9e7e7a74b3db6ec27
tamarit <tamarit27@gmail.com>
2016-08-10 18:58:39 +0200

    euler025
    https://www.hackerrank.com/contests/projecteuler/challenges/euler025

---
+++ euler025/solution.erl
@@ -0,0 +1,108 @@
+% Enter your code here. Read input from STDIN. Print output to STDOUT
+% Your class should be named solution
+
+% https://www.hackerrank.com/contests/projecteuler/challenges/euler025
+
+-module(solution).
+-export([main/0, prof/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+main() ->
+    Data = read_data(),
+    Res = calculate(Data),
+    output_data(Res),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Profiling
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+prof() ->
+    eprof:start(),
+    eprof:start_profiling([self()]),
+    main2(),
+    eprof:stop_profiling(),
+    eprof:analyze(total). 
+
+main2() ->
+    StartInput = os:timestamp(),
+    Data = read_data(),
+    io:format("INPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartInput)/1000000]),
+    StartRes = os:timestamp(),
+    Res = calculate(Data),
+    io:format("CALCULATE: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartRes)/1000000]),
+    StartOutput = os:timestamp(),
+    output_data(Res),
+    io:format("OUTPUT: total time taken ~p seconds~n", [timer:now_diff(os:timestamp(), StartOutput)/1000000]),
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+calculate(Ts) ->
+    lists:map(fun n_digit/1, Ts).
+
+n_digit(N) ->
+    Phi = (1 + math:sqrt(5)) / 2,
+    ceiling(((math:log(10) * (N - 1)) + (math:log(5) / 2)) / math:log(Phi)).
+
+
+% *********************************
+% BEGIN Copied from https://erlangcentral.org/wiki/index.php?title=Floating_Point_Rounding
+% *********************************
+
+ceiling(X) when X < 0 ->
+    trunc(X);
+ceiling(X) ->
+    T = trunc(X),
+    case X - T == 0 of
+        true -> T;
+        false -> T + 1
+    end.
+
+% *********************************
+% END Copied from https://erlangcentral.org/wiki/index.php?title=Floating_Point_Rounding
+% *********************************
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Output
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+output_data(Ds) -> 
+    lists:map(
+        fun(D) -> io:format("~p\n", [D]) end, 
+        Ds).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+read_data() ->
+    Binary = read(),
+    Res = binary:split(Binary, [<<"\n">>], [global]),
+    [_| Tests] = [binary_to_list(R) || R <- Res],
+    lists:map(fun str2int/1, Tests).
+
+str2int(Str) ->
+    element(1,string:to_integer(Str)).
+
+-define(BLK_SIZE, 16384).
+
+read() ->
+    ok = io:setopts(standard_io, [binary]),
+    read(<<>>).
+
+read(Acc) ->
+    case file:read(standard_io, ?BLK_SIZE) of
+        {ok, Data} ->
+            read(<<Acc/bytes, Data/bytes>>);
+        eof ->
+            Acc
+    end.
+
+ed(T) ->    
+    erlang:display(T).
